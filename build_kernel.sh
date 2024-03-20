3844c0abb0f2c9355835baa72170dfe767d28ddb
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-03-20 21:03:31 +0100

    build: apply patch to KernelSU

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-
+clear
 export ARCH=arm64
-export PLATFORM_VERSION=12
-export ANDROID_MAJOR_VERSION=s
+export PLATFORM_VERSION=13
+export ANDROID_MAJOR_VERSION=t
+ln -s /usr/bin/python2.7 $HOME/bin/python
+export PATH=$HOME/bin/:$PATH
 
-make ARCH=arm64 exynos9610-gta4xlxx_defconfig
-make ARCH=arm64 -j16
+ARGS='
+CC=/home/federico/Projects/clang-4639204/bin/clang
+CROSS_COMPILE=/home/federico/Projects/Toolchains_for_Snapdragon/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+CLANG_TRIPLE=aarch64-linux-gnu-
+ARCH=arm64
+'
+
+make ${ARGS} clean && make ${ARGS} mrproper #to clean the source
+make ${ARGS} exynos9610-gta4xlxx_defconfig #making your current kernel config
+#make ${ARGS} menuconfig
+make ${ARGS} -j2 #to compile the kernel
+
+#to copy all the kernel modules (.ko) to "modules" folder.
+mkdir -p modules
+find . -type f -name "*.ko" -exec cp -n {} modules \;
+echo "Module files copied to the 'modules' folder."
